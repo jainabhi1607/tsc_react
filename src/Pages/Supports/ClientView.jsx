8be0118ec8930fe5f49a0eb2fd48eb91c8c79058
Shankey Jain <shankey@sxda.com.au>
2025-08-17 19:43:22 +0530

    create client view page

--- src/Pages/Supports/ClientView.jsx
+++ src/Pages/Supports/ClientView.jsx
@@ -57,39 +57,39 @@ let optionsTime = {
       {data?.supportClientViewData?.details?.ticket_no !== ""}
       {
         <div>
-          <h1 class="page-heading marB58">
-            <a href="support" class="back-icon"></a>
+          <h1 className="page-heading marB58">
+            <a href="support" className="back-icon"></a>
             <span>
               Support Ticket # {data?.supportClientViewData?.details?.ticket_no}
             </span>
           </h1>
-          <div class="col-lg-8 paddL0">
-            <div class="table-responsive">
-              <div class="paddT5">
-                <span class="client_name font-26">
+          <div className="col-lg-8 paddL0">
+            <div className="table-responsive">
+              <div className="paddT5">
+                <span className="client_name font-26">
                   {data?.supportClientViewData?.details?.Clients?.company_name}
                 </span>
               </div>
-              <div class="clear paddB10"></div>
-              <div class="clear paddB5">
+              <div className="clear paddB10"></div>
+              <div className="clear paddB5">
                 {data?.supportClientViewData?.details?.ClientSites.site_name} -{" "}
                 {data?.supportClientViewData?.details?.ClientSites.address}
               </div>
-              <div class="clear paddB5">
+              <div className="clear paddB5">
                 {data?.supportClientViewData?.details?.ClientContacts.name}{" "}
                 {data?.supportClientViewData?.details?.ClientContacts.last_name}
               </div>
-              <div class="clear paddB5">
+              <div className="clear paddB5">
                 {data?.supportClientViewData?.details?.ClientContacts.phone}
               </div>
-              <div class="clear paddB5">
+              <div className="clear paddB5">
                 {data?.supportClientViewData?.details?.ClientContacts.email}
               </div>
-              <div class="clear paddB10"></div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
-                <label class="normal gray-color">Sites</label>
-                <label class="site-right">
+              <div className="clear paddB10"></div>
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
+                <label className="normal gray-color">Sites</label>
+                <label className="site-right">
                   <span>
                     {
                       data?.supportClientViewData?.details?.ClientSites
@@ -98,10 +98,10 @@ let optionsTime = {
                   </span>
                 </label>
               </div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
-                <label class="normal gray-color">Asset</label>
-                <label class="site-right">
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
+                <label className="normal gray-color">Asset</label>
+                <label className="site-right">
                   {data?.supportClientViewData?.details?.ClientAssets
                     .machine_name
                     ? data?.supportClientViewData?.details?.ClientAssets
@@ -112,34 +112,34 @@ let optionsTime = {
                     : ""}
                 </label>
               </div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
-                <label class="normal gray-color">Production Impact</label>
-                <label class="float-right">
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
+                <label className="normal gray-color">Production Impact</label>
+                <label className="float-right">
                   {data?.supportClientViewData?.details?.production_impact}
                 </label>
               </div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
-                <label class="normal gray-color">Time issue occured</label>
-                <label class="float-right">
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
+                <label className="normal gray-color">Time issue occured</label>
+                <label className="float-right">
                   {data?.supportClientViewData?.details?.time_issue_hours}:
                   {data?.supportClientViewData?.details?.time_issue_minutes}
                   {data?.supportClientViewData?.details?.time_issue_ampm}
                 </label>
               </div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
-                <label class="normal gray-color">Date Submitted</label>
-                <label class="float-right">
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
+                <label className="normal gray-color">Date Submitted</label>
+                <label className="float-right">
                   {data?.supportClientViewData?.details?.date_time}
                 </label>
               </div>
-              <div class="clear popup-divider"></div>
-              <div class="col-lg-12 paddR0 paddL0">
+              <div className="clear popup-divider"></div>
+              <div className="col-lg-12 paddR0 paddL0">
                 <h4>Description</h4>
-                <div class="clear paddB5"></div>
-                <div class="col-lg-12 paddL0 paddR0 popup-input width100 line-22">
+                <div className="clear paddB5"></div>
+                <div className="col-lg-12 paddL0 paddR0 popup-input width100 line-22">
                   {data?.supportClientViewData?.details?.SupportTicketDetails
                     .description ? (
                     data?.supportClientViewData?.details?.SupportTicketDetails
@@ -151,9 +151,9 @@ let optionsTime = {
               </div>
             </div>
 
-            <div class="col-lg-12 paddL0 paddB10 paddR0 lead-note-section comment_section">
+            <div className="col-lg-12 paddL0 paddB10 paddR0 lead-note-section comment_section">
               <form method="post">
-                <h3 class="paddB10">
+                <h3 className="paddB10">
                   {userRole >= 4 ? "Activity" : "Comments / Updates"}
                 </h3>
                 <div>
@@ -161,18 +161,18 @@ let optionsTime = {
                     placeholder="Start typing..."
                     name="comments"
                     id="comment"
-                    class="note_input"
+                    className="note_input"
                     rows="4"
                     required
                   ></textarea>
-                  <div class="clear paddB10"></div>
+                  <div className="clear paddB10"></div>
                   <input
                     type="submit"
                     className="blue-btn float-left marR20"
                     id="submit_btn"
                     value="Save"
                   />
-                  <div class="clear paddB10"></div>
+                  <div className="clear paddB10"></div>
 
                   {userData?.role < 4 && (
                     <>
@@ -183,13 +183,13 @@ let optionsTime = {
                         id="public_private"
                         className="public_private_class"
                       />
-                      <label for="public_private" class="public_private_class">
+                      <label for="public_private" className="public_private_class">
                         Make Public
                       </label>
                     </>
                   )}
 
-                  <div class="clear paddB30"></div>
+                  <div className="clear paddB30"></div>
                 </div>
               </form>
               {console.log(data?.supportClientViewData?.comments)}
@@ -225,9 +225,9 @@ let optionsTime = {
                       return (
                         <>
                           <div className={className}>
-                            <span class="blue-color">{final_date}</span>
-                            <div class="clear paddB10"></div>
-                            <div class="black-color">{message}</div>
+                            <span className="blue-color">{final_date}</span>
+                            <div className="clear paddB10"></div>
+                            <div className="black-color">{message}</div>
                           </div>
                         </>
                       );
@@ -236,51 +236,51 @@ let optionsTime = {
             </div>
           </div>
 
-          <div class="col-lg-4 paddR0">
-            <div class="table-responsive job-card-sidebar">
-               <h3 class="bold600">Ticket Progress</h3>
-               <div class="clear paddB10"></div>
-               <div class="ticket_progress">
-                      <span class="ticket_circle"></span>
-                    <span>Ticket Created<br/><span class="paddL28">
+          <div className="col-lg-4 paddR0">
+            <div className="table-responsive job-card-sidebar">
+               <h3 className="bold600">Ticket Progress</h3>
+               <div className="clear paddB10"></div>
+               <div className="ticket_progress">
+                      <span className="ticket_circle"></span>
+                    <span>Ticket Created<br/><span className="paddL28">
                       {data?.supportClientViewData?.details?.dat_time}
                       </span></span>
-                    <div class="clear paddB10"></div>
-                    <span class="left-border"></span>
-                    <div class="clear paddB25"></div>
+                    <div className="clear paddB10"></div>
+                    <span className="left-border"></span>
+                    <div className="clear paddB25"></div>
                     
                       <span className={`ticket_circle ${data?.supportClientViewData?.details?.ticket_status < 2 ? "ticket_disabled" : "red-circle"}`}></span>
-                    <span class={`${data?.supportClientViewData?.details?.ticket_status < 2 ? "ticket_disabled" :""}`}>In Progress</span>
-                    <div class="clear paddB25"></div>
-                    <span class="left-border"></span>
-                    <div class="clear paddB25"></div>
+                    <span className={`${data?.supportClientViewData?.details?.ticket_status < 2 ? "ticket_disabled" :""}`}>In Progress</span>
+                    <div className="clear paddB25"></div>
+                    <span className="left-border"></span>
+                    <div className="clear paddB25"></div>
 
                       <span className={`ticket_circle ${data?.supportClientViewData?.details?.ticket_status < 3 ? "ticket_disabled" : "yellow-circle"}`}></span>
 
-                    <span class={`${data?.supportClientViewData?.details?.ticket_status < 3 ? "ticket_disabled" :""}`}>Technician Required<br/>
+                    <span className={`${data?.supportClientViewData?.details?.ticket_status < 3 ? "ticket_disabled" :""}`}>Technician Required<br/>
                     
                     
-                    <span class="paddL28">{data?.supportClientViewData?.details?.ticket_status >= 3 && data?.supportClientViewData?.details?.JobCards?.job_date !== '' ? "Job Date: "+data.supportClientViewData.details.JobCards.job_date :""}
+                    <span className="paddL28">{data?.supportClientViewData?.details?.ticket_status >= 3 && data?.supportClientViewData?.details?.JobCards?.job_date !== '' ? "Job Date: "+data.supportClientViewData.details.JobCards.job_date :""}
                     
                     </span></span>
-                    <div class="clear paddB10"></div>
-                    <span class="left-border"></span>
-                    <div class="clear paddB25"></div>
+                    <div className="clear paddB10"></div>
+                    <span className="left-border"></span>
+                    <div className="clear paddB25"></div>
 
                       <span className={`ticket_circle ${data?.supportClientViewData?.details?.ticket_status < 4 ? "ticket_disabled" : "blue-circle"}`}></span>
 
-                    <span class={`${data?.supportClientViewData?.details?.ticket_status < 4 ? "ticket_disabled" :""}`}>Ticket Closed<br/>
+                    <span className={`${data?.supportClientViewData?.details?.ticket_status < 4 ? "ticket_disabled" :""}`}>Ticket Closed<br/>
                     
-                    <span class="paddL28">                         
+                    <span className="paddL28">                         
                          {data?.supportClientViewData?.details?.ticket_status >= 4 && data?.supportClientViewData?.details?.SupportTicketDetails?.resolved_date !== '' ? data.supportClientViewData.details.SupportTicketDetails.resolved_date :""}
                          </span></span>
                  </div>
             </div>
-            <div class="table-responsive">
-               <label class="job-sub-heading width100">Attachments <span class="float-right normal gray-color">Total </span></label>
-               <div class="clear paddB10"></div>
+            <div className="table-responsive">
+               <label className="job-sub-heading width100">Attachments <span className="float-right normal gray-color">Total </span></label>
+               <div className="clear paddB10"></div>
                
-               <div class="clear paddB10"></div>
+               <div className="clear paddB10"></div>
             </div>
          </div>
 
